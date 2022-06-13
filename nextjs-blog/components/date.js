48cd9f3aa45d84b8a4393d8ac60aabba197f5cca
João Walter Amadeu <joao.amadeu@unifio.edu.br>
2022-06-13 19:04:47 -0300

    Data mais bonitinha

--- nextjs-blog/components/date.js
+++ nextjs-blog/components/date.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns'
-
+import { ptBR } from 'date-fns/locale'
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, ' dd LLLL yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, "eeee, dd 'de' MMMM 'de' yyyy", { locale: ptBR })}</time>
 }
